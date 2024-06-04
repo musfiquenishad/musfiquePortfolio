fecdea6e64fae2c6da8d9697d697b289ef6ebf35
musfiquenishad <ahsannishad@gmail.com>
2024-06-04 23:41:31 +0600

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,182 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="main bg-zinc-900">
+      {/* Header */}
+
+      <div className="flex items-center justify-center">
+        <nav className="pointer-events-auto mt-8 hidden md:block">
+          <ul className="flex rounded-full bg-zinc-800/90 px-3 text-sm font-medium text-zinc-200 shadow-lg shadow-zinc-800/5 ring-1 ring-white/10 backdrop-blur">
+            <li>
+              <Link
+                className="relative block px-3 py-2 transition hover:text-teal-400"
+                href="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="relative block px-3 py-2 transition hover:text-teal-400"
+                href="/projects"
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="relative block px-3 py-2 transition hover:text-teal-400"
+                href="/articles"
+              >
+                Articles
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="relative block px-3 py-2 transition hover:text-teal-400"
+                href="/contact"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="sm:px-8 mt-16">
+        <div className="mx-auto w-full max-w-7xl lg:px-8">
+          <div className="relative px-4 sm:px-8 lg:px-12">
+            <div className="mx-auto max-w-2xl lg:max-w-5xl">
+              <div className="max-w-2xl">
+                <Image
+                  className="rounded-full"
+                  src="/profile.jpg"
+                  width={64}
+                  height={64}
+                  alt="Picture of the author"
+                />
+                <h1 className="text-4xl font-bold tracking-tight text-zinc-100 sm:text-5xl mt-9 sm:leading-normal">
+                  Software designer, founder, and amateur inventor.
+                </h1>
+                <p className="mt-6 text-base text-zinc-400 ">
+                  I’m Musfique, a software designer and entrepreneur based in
+                  Dhaka, Bangladesh. I develop technologies that empower regular
+                  people to explore internet on their own terms. I focus on
+                  crafting innovative technologies that enable ordinary people
+                  to navigate and use the internet according to their
+                  preferences.
+                </p>
+
+                <div className="mt-6 flex gap-6">
+                  <div className="mt-2 flex items-center justify-center gap-x-6 lg:justify-start">
+                    <Link
+                      href="/projects"
+                      className="text-sm font-semibold leading-6 text-teal-500"
+                    >
+                      Projects <span aria-hidden="true">→</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="mt-16 sm:mt-20">
+        <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+          <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl bg-zinc-800 rotate-2">
+            <Image
+              className="absolute inset-0 h-full w-full object-cover"
+              src="/image1.jpeg"
+              width={3744}
+              height={5616}
+              alt="Picture of the author"
+            />
+          </div>
+          <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl bg-zinc-800 -rotate-2">
+            <Image
+              className="absolute inset-0 h-full w-full object-cover"
+              src="/image2.jpeg"
+              width={3936}
+              height={2624}
+              alt="Picture of the author"
+            />
+          </div>
+          <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl bg-zinc-800 rotate-2">
+            <Image
+              className="absolute inset-0 h-full w-full object-cover"
+              src="/image7.jpg"
+              width={5760}
+              height={3840}
+              alt="Picture of the author"
+            />
+          </div>
+          <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl bg-zinc-800 -rotate-2">
+            <Image
+              className="absolute inset-0 h-full w-full object-cover"
+              src="/image4.jpg"
+              width={2400}
+              height={3000}
+              alt="Picture of the author"
+            />
+          </div>
+          <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl bg-zinc-800 rotate-2">
+            <Image
+              className="absolute inset-0 h-full w-full object-cover"
+              src="/image5.webp"
+              width={3120}
+              height={4160}
+              alt="Picture of the author"
+            />
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className="mt-32 flex-none">
+        <div className="sm:px-8">
+          <div className="mx-auto w-full max-w-7xl lg:px-8">
+            <div className="border-t border-zinc-700/40 pb-16 pt-10">
+              <div className="relative px-4 sm:px-8 lg:px-12">
+                <div className="mx-auto max-w-2xl lg:max-w-5xl">
+                  <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+                    <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-100">
+                      <Link
+                        href="/about"
+                        className="transition hover:text-teal-400"
+                      >
+                        About
+                      </Link>
+                      <Link
+                        href="/projects"
+                        className="transition hover:text-teal-400"
+                      >
+                        Projects
+                      </Link>
+                      <Link
+                        href="/articles"
+                        className="transition hover:text-teal-400"
+                      >
+                        Articles
+                      </Link>
+                      <Link
+                        href="/contact"
+                        className="transition hover:text-teal-400"
+                      >
+                        Contact
+                      </Link>
+                    </div>
+                    <p className="text-sm text-zinc-500">
+                      © 2024 Musfique All rights reserved.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 }
